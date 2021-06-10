c1784dea25edf0a8b5c8be072b33c862369c2d67
liuboringbook <1287128847@qq.com>
2021-06-10 16:20:29 +0800

    first commit

---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+			<!-- 自定义组件 -->
+			<navbar></navbar>
+		</view>
+	</view>
+</template>
+
+<script>
+	//easyCom components/组件名/组件名.vue  局部引入
+	import navbar from '@/components/navbar/navbar.vue'
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		components:{
+			navbar
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
